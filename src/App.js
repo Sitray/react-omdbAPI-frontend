b751b9bb6d63c4d62619d547bd5395e90655d1fb
Eric <ericmares13@gmail.com>
2020-02-19 17:21:57 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import { Router, Route } from 'react-router';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 import HomeComponent from './components/HomeComponent';
-
-function App() {
-  return (
-    <div>
-      <HomeComponent />
-    </div>
-  );
+import MovieCardComponent from './components/MovieCardComponent';
+export default class extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={() => <HomeComponent />} />
+          <Route path="/movie" render={() => <MovieCardComponent />} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
