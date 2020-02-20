8bea70240840d7cfdc929a359db64a53c61901fa
Eric <ericmares13@gmail.com>
2020-02-20 14:02:14 +0100

    useFetch hook

---
+++ src/hooks.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url, options) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url, options);
+        const json = await response.json();
+        setData(json);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return { data, error, isLoading };
+};
+
+export { useFetch };
