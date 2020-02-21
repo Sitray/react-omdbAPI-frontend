f814a3e82b38c78f4caaa9b5b1aa12ec1d6cc9c3
Eric <ericmares13@gmail.com>
2020-02-21 12:56:35 +0100

    delete unnecessary lines

--- src/reducer.js
+++ src/reducer.js
@@ -7,13 +7,11 @@ export default (state = initialState, action) => {
   let newState = JSON.parse(JSON.stringify(state));
   switch (action.type) {
     case 'SET_MOVIE':
-      newState.movie = '';
       newState.movie = action.item;
       console.log(newState.movie);
       return newState;
 
     case 'SET_MOVIE_ID':
-      newState.movieId = '';
       newState.movieId = action.item;
       console.log(newState.movieId + 'movieId');
       return newState;
